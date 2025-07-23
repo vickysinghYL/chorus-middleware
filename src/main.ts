c44a63906b84082e3ddde01d9989b5a5d94a87c7
vickysinghYL <vicky.singh@ylconsulting.com>
2025-07-24 01:17:25 +0530

    increase json limit

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     
     // Set global prefix for all routes
     app.setGlobalPrefix('api');
